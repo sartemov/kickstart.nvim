a96b3ac2fa9d33c7f4f634a2edbdb7596c63ee35
sartemov <artemov.sergei@gmail.com>
2023-11-19 20:20:53 +0300

    update

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,17 +1,28 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+   -- your configuration comes here
+   -- or leave it empty to use the default settings
+   -- refer to the configuration section below
+  },
 
- },
-  -- Lua
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end),
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end),
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end),
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end),
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end),
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end),
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+),
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+),
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  {silent = true, noremap = true}
+),
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  {silent = true, noremap = true}
+),
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  {silent = true, noremap = true}
+),
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  {silent = true, noremap = true}
+),
 }
